192a57a20a1aaf39f1f5ff93fc2e27f4d771422c
yml <m18538527269@163.com>
2026-05-28 17:02:34 +0800

    第二次初始化前端项目

---
+++ test3.js
@@ -0,0 +1,15 @@
+function createGreeting(name) {
+  const greeting = `你好，${name}！`;
+  return greeting;
+}
+
+const helloChris = createGreeting("Chris");
+
+console.log(helloChris);
+
+function sayHello(name) {
+  const greeting = `你好，${name}！`;
+  return greeting;
+}
+
+const a={name:"Tom"}
