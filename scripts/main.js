865967a89bbbfbc564c060f7f60a7345df46e4e3
yml <m18538527269@163.com>
2026-05-29 10:03:12 +0800

    修复点击图片不能切换的问题

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,39 @@
-const myImage=document.querySelector("img");
-myImage.addEventListener("click",()=>{
-    const mySrc=myImage.getAttribute("src");
-    if(mySrc==="images/firefox-icon.png"){
-        myImage.setAttribute("src","images/firefox2.png");
+// Image switcher code
 
-    }else{
-        myImage.setAttribute("src","images/firsfox-icon.png");
-    }
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
 });
 
-let myButton=document.querySelector('button');
-let myHeading=document.querySelector('h1');
 
-function setUserName(){
-    let myName=promt('please enter your name');
-    if (!myName){
-        setUserName();
-    }else{
-        localStorage.setItem('name',myName);
-        myHeading.textContent='Mozilla is cool, ' + myName;
-    }
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
 }
 
-if (!localStorage.getItem('name')){
-    setUserName();
-
-}else{
-    let storedName=localStorage.getItem('name');
-    myHeading.textContent='Mozilla is cool, ' + storedName;
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
 }
 
-myButton.addEventListener("click",()=>{
-    setUserName();
-})
+myButton.addEventListener("click", () => {
+  setUserName();
+});
