8b6cecedce3c4ca0ad5c7147a05850c47192f1d5
yml <m18538527269@163.com>
2026-05-29 16:51:14 +0800

    添加html中title元素2

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,9 @@ myImage.addEventListener("click", () => {
   } else {
     myImage.setAttribute("src", "images/firefox-icon.png");
   }
+//   else {
+//     myImage.setAttribute("src", "images/firefox3.png");
+//   }
 });
 
 
@@ -37,3 +40,28 @@ if(!localStorage.getItem('name')) {
 myButton.addEventListener("click", () => {
   setUserName();
 });
+
+
+
+
+const list = document.createElement('ul');
+const info = document.createElement('p');
+const html = document.querySelector('html');
+
+info.textContent = 'Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.';
+
+document.body.appendChild(info);
+document.body.appendChild(list);
+
+html.onclick = function() {
+  const listItem = document.createElement('li');
+  const listContent = prompt('What content do you want the list item to have?');
+  listItem.textContent = listContent;
+  list.appendChild(listItem);
+
+  listItem.onclick = function(e) {
+    e.stopPropagation();
+    const listContent = prompt('Enter new content for your list item');
+    this.textContent = listContent;
+  }
+}
