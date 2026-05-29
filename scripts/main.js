2f95c7a7234a56f563b083329c7602a050796d9e
yml <m18538527269@163.com>
2026-05-29 17:43:32 +0800

    练习：斜体强调和着重强调的词上添加斜体强调和着重强调的内容

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,122 @@
-// Image switcher code
+// // Image switcher code
 
-const myImage = document.querySelector("img");
+// const myImage = document.querySelector("img");
 
-myImage.addEventListener("click", () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
-  } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
-  }
-//   else {
-//     myImage.setAttribute("src", "images/firefox3.png");
+// myImage.addEventListener("click", () => {
+//   const mySrc = myImage.getAttribute("src");
+//   if (mySrc === "images/firefox-icon.png") {
+//     myImage.setAttribute("src", "images/firefox2.png");
+//   } else {
+//     myImage.setAttribute("src", "images/firefox-icon.png");
 //   }
+// //   else {
+// //     myImage.setAttribute("src", "images/firefox3.png");
+// //   }
+// });
+
+
+// // Personalized welcome message code
+
+// let myButton = document.querySelector('button');
+// let myHeading = document.querySelector('h1');
+
+// function setUserName() {
+//   let myName = prompt('Please enter your name.');
+//   if(!myName) {
+//     setUserName();
+//   } else {
+//     localStorage.setItem('name', myName);
+//     myHeading.textContent = 'Mozilla is cool, ' + myName;
+//   }
+// }
+
+// if(!localStorage.getItem('name')) {
+//   setUserName();
+// } else {
+//   let storedName = localStorage.getItem('name');
+//   myHeading.textContent = 'Mozilla is cool, ' + storedName;
+// }
+
+// myButton.addEventListener("click", () => {
+//   setUserName();
+// });
+const textarea = document.getElementById("code");
+const reset = document.getElementById("reset");
+const solution = document.getElementById("solution");
+const output = document.querySelector(".output");
+const code = textarea.value;
+let userEntry = textarea.value;
+
+function updateCode() {
+  output.innerHTML = textarea.value;
+}
+
+const htmlSolution = `<h1>AlphaGo 李世乭五番棋</h1>
+
+<p><strong>2016 年 3 月 8 日</strong>到<strong>3 月 15 日</strong>，韩国职业棋士<strong>李世乭（이세돌）<em>九段</em></strong>与由 Google DeepMind 开发的计算机围棋软件 <strong>AlphaGo</strong> 对弈的五局三胜制围棋比赛在韩国<strong>首尔</strong>举行。结果为 AlphaGo 以<strong>四胜一负</strong>的战绩击败李世乭。赛后韩国棋院授予 AlphaGo <strong>荣誉九段</strong>的称号。</p>`;
+let solutionEntry = htmlSolution;
+
+reset.addEventListener("click", () => {
+  textarea.value = code;
+  userEntry = textarea.value;
+  solutionEntry = htmlSolution;
+  solution.value = "显示答案";
+  updateCode();
 });
 
-
-// Personalized welcome message code
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-    setUserName();
+solution.addEventListener("click", () => {
+  if (solution.value === "显示答案") {
+    textarea.value = solutionEntry;
+    solution.value = "隐藏答案";
   } else {
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
+    textarea.value = userEntry;
+    solution.value = "显示答案";
   }
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.addEventListener("click", () => {
-  setUserName();
+  updateCode();
 });
+
+textarea.addEventListener("input", updateCode);
+window.addEventListener("load", updateCode);
+
+// 阻止制表键跳出文本区域
+// 而是在光标位置输出制表符
+textarea.onkeydown = (e) => {
+  if (e.code === "Tab") {
+    e.preventDefault();
+    insertAtCaret("\t");
+  }
+
+  if (e.code === "Escape") {
+    textarea.blur();
+  }
+};
+
+function insertAtCaret(text) {
+  const scrollPos = textarea.scrollTop;
+  let caretPos = textarea.selectionStart;
+
+  const front = textarea.value.substring(0, caretPos);
+  const back = textarea.value.substring(
+    textarea.selectionEnd,
+    textarea.value.length,
+  );
+  textarea.value = front + text + back;
+  caretPos += text.length;
+  textarea.selectionStart = caretPos;
+  textarea.selectionEnd = caretPos;
+  textarea.focus();
+  textarea.scrollTop = scrollPos;
+}
+
+// 每次用户更新文本区域代码时，更新已保存的 userCode
+textarea.onkeyup = () => {
+  // 我们只想在显示用户代码时保存状态，
+  // 而不是保存解答，因此解答不会保存在用户代码上
+  if (solution.value === "显示答案") {
+    userEntry = textarea.value;
+  } else {
+    solutionEntry = textarea.value;
+  }
+
+  updateCode();
+};
